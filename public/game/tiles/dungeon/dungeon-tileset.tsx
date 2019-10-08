1c7bc820d41b636c0af43d7087c0fc6c27d56aea
Trezy <tre@trezy.com>
2019-10-08 00:34:34 -0500

    feat: add the test dungeon

--- public/game/tiles/dungeon/dungeon-tileset.tsx
+++ public/game/tiles/dungeon/dungeon-tileset.tsx
@@ -4,27 +4,169 @@
  <terraintypes>
   <terrain name="Floor" tile="-1"/>
   <terrain name="Abyss" tile="0"/>
+  <terrain name="Stone Tile" tile="0"/>
  </terraintypes>
  <tile id="30" terrain="1,1,1,1"/>
- <tile id="31" terrain=",,,1"/>
- <tile id="32" terrain=",,1,"/>
- <tile id="33" terrain=",1,,"/>
- <tile id="34" terrain="1,,,"/>
- <tile id="35" terrain=",1,,1"/>
- <tile id="36" terrain=",1,,1"/>
- <tile id="37" terrain=",1,,1"/>
- <tile id="38" terrain=",,1,1"/>
- <tile id="39" terrain=",,1,1"/>
- <tile id="40" terrain=",,1,1"/>
- <tile id="41" terrain="1,1,,"/>
- <tile id="42" terrain="1,1,,"/>
- <tile id="43" terrain="1,1,,"/>
- <tile id="44" terrain="1,,1,"/>
- <tile id="45" terrain="1,,1,"/>
- <tile id="46" terrain="1,,1,"/>
- <tile id="80" terrain="1,,1,1"/>
- <tile id="81" terrain="1,1,,1"/>
- <tile id="82" terrain="1,1,1,"/>
+ <tile id="31" terrain=",,,1">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="3" height="32"/>
+   <object id="2" type="boundary" x="0" y="0" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="32" terrain=",,1,">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="29" y="0" width="3" height="32"/>
+   <object id="2" type="boundary" x="0" y="0" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="33" terrain=",1,,">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="3" height="32"/>
+   <object id="2" type="boundary" x="0" y="0" width="3" height="32"/>
+   <object id="3" type="boundary" x="0" y="29" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="34" terrain="1,,,">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="29" y="0" width="3" height="32"/>
+   <object id="2" type="boundary" x="0" y="29" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="35" terrain=",1,,1">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="36" terrain=",1,,1">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="37" terrain=",1,,1">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="38" terrain=",,1,1">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="39" terrain=",,1,1">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="40" terrain=",,1,1">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="41" terrain="1,1,,">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="42" terrain="1,1,,">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="43" terrain="1,1,,">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="44" terrain="1,,1,">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="45" terrain="1,,1,">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="46" terrain="1,,1,">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="79" terrain=",1,1,1">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="0" y="0" width="3" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="80" terrain="1,,1,1">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="29" y="0" width="3" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="81" terrain="1,1,,1">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="3" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="82" terrain="1,1,1,">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="29" y="29" width="3" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="120">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="125">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="126">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="29" y="0" width="3" height="32"/>
+   <object id="2" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="150">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+   <object id="2" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="151">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="152">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="153">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="154">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="155">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+   <object id="2" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="156">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+   <object id="2" type="boundary" x="29" y="0" width="3" height="32"/>
+   <object id="3" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="210" terrain="0,0,0,0"/>
  <tile id="211" terrain=",,,0"/>
  <tile id="212" terrain=",,0,"/>
@@ -58,6 +200,69 @@
  <tile id="276" terrain="0,0,0,0"/>
  <tile id="277" terrain="0,0,0,0"/>
  <tile id="278" terrain="0,0,0,0"/>
+ <tile id="279" terrain="2,2,2,2"/>
+ <tile id="280" terrain="2,2,2,2"/>
+ <tile id="281" terrain="2,2,2,2"/>
+ <tile id="330">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="331">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="332">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="333">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="334">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="335">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="337">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="2" y="2" width="6" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="338">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="24" y="2" width="6" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="339">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="2" y="2" width="6" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="340">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="24" y="2" width="6" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="341">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="0" y="0" width="6" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="342">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="26" y="0" width="6" height="32"/>
+  </objectgroup>
+ </tile>
  <tile id="348">
   <animation>
    <frame tileid="348" duration="1000"/>
@@ -75,6 +280,72 @@
    <frame tileid="352" duration="1000"/>
    <frame tileid="353" duration="1000"/>
   </animation>
+ </tile>
+ <tile id="360">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+   <object id="2" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="361">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+   <object id="2" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="362">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+   <object id="2" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="363">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+   <object id="2" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="364">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+   <object id="2" type="boundary" x="0" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="365">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="29" width="32" height="3"/>
+   <object id="2" type="boundary" x="29" y="0" width="3" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="367">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="2" y="0" width="6" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="368">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="24" y="0" width="6" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="369">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="2" y="0" width="6" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="370">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="24" y="0" width="6" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="371">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="0" y="0" width="6" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="372">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="26" y="0" width="6" height="32"/>
+  </objectgroup>
  </tile>
  <tile id="378">
   <animation>
@@ -94,12 +365,192 @@
    <frame tileid="383" duration="1000"/>
   </animation>
  </tile>
+ <tile id="397">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="2" y="0" width="6" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="398">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="24" y="0" width="6" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="399">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="2" y="0" width="6" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="400">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="24" y="0" width="6" height="30"/>
+  </objectgroup>
+ </tile>
+ <tile id="401">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="6" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="402">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="26" y="0" width="6" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="468">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="26" width="32" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="469">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="26" width="32" height="6"/>
+  </objectgroup>
+ </tile>
+ <tile id="473">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="13" width="32" height="19"/>
+  </objectgroup>
+ </tile>
+ <tile id="493">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="8" y="14" width="16" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="494">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="8" y="14" width="16" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="495">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="6" y="12" width="20" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="496">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="6" y="12" width="20" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="498">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="499">
+  <objectgroup draworder="index">
+   <object id="2" type="boundary" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="503">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="533">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="0" width="32" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="570">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="4" y="16" width="24" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="571">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="4" y="16" width="24" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="572">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="20" width="32" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="573">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="20" width="32" height="12"/>
+  </objectgroup>
+ </tile>
+ <tile id="574">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="6" y="18" width="20" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="575">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="6" y="18" width="20" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="576">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="8" y="22" width="16" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="577">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="6" y="22" width="20" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="578">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="2" y="20" width="28" height="12"/>
+  </objectgroup>
+ </tile>
  <tile id="581">
   <animation>
    <frame tileid="581" duration="200"/>
    <frame tileid="582" duration="200"/>
    <frame tileid="583" duration="200"/>
   </animation>
+ </tile>
+ <tile id="585">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="4" y="22" width="24" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="586">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="4" y="22" width="24" height="10"/>
+  </objectgroup>
+ </tile>
+ <tile id="619">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="10" width="32" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="620">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="10" width="32" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="621">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="10" width="32" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="622">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="10" width="32" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="624">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="10" width="32" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="625">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="10" width="32" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="626">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="10" width="32" height="22"/>
+  </objectgroup>
+ </tile>
+ <tile id="627">
+  <objectgroup draworder="index">
+   <object id="1" type="boundary" x="0" y="10" width="32" height="22"/>
+  </objectgroup>
  </tile>
  <tile id="974">
   <animation>
