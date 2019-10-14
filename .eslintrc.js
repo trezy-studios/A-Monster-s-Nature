9338a1b8b2f8975e37d5a4024e8306831c9495a9
Trezy <tre@trezy.com>
2019-10-13 22:19:38 -0500

    chore: update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
   extends: '@fuelrats/eslint-config-react',
   rules: {
     'arrow-parens': ['error', 'as-needed'],
+    'default-case': ['off'],
     'max-len': ['off'],
+    'max-statements': ['off'],
     'new-parens': ['error', 'never'],
     'semi-style': ['off'],
 
